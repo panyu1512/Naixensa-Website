224afd9118393a78914c7f799460221c8ed73fc5
panyu1512 <kikeferreragius1997@gmail.com>
2023-10-31 15:11:45 +0100

    create logo name properties

--- components/primitives.ts
+++ components/primitives.ts
@@ -1,5 +1,43 @@
 import { tv } from "tailwind-variants";
 
+export const logoName = tv({
+	base: "tracking-tight inline font-semibold",
+	variants: {
+		color: {
+			violet: "from-[#7828c8] to-[#cd9be2]",
+			yellow: "from-[#FF705B] to-[#FFB457]",
+			blue: "from-[#5EA2EF] to-[#0072F5]",
+			cyan: "from-[#00b7fa] to-[#01cfea]",
+			green: "from-[#6FEE8D] to-[#17c964]",
+			pink: "from-[#FF72E1] to-[#F54C7A]",
+			foreground: "dark:from-[#FFFFFF] dark:to-[#4B4B4B]",
+		},
+		size: {
+			sm: "text-lg lg:text-lg",
+			md: "text-[2.3rem] lg:text-5xl leading-9",
+			lg: "text-4xl lg:text-6xl",
+		},
+
+	},
+	defaultVariants: {
+		size: "sm",
+	},
+	compoundVariants: [
+		{
+			color: [
+				"violet",
+				"yellow",
+				"blue",
+				"cyan",
+				"green",
+				"pink",
+				"foreground",
+			],
+			class: "bg-clip-text text-transparent bg-gradient-to-b",
+		},
+	],
+});
+
 export const title = tv({
 	base: "tracking-tight inline font-semibold",
 	variants: {
