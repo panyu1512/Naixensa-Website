b1295307d39fcdef0e1587d4600ade19dc153bc0
panyu1512 <kikeferreragius1997@gmail.com>
2023-11-04 01:37:43 +0100

    Add User and Token interfaces.

---
+++ app/interfaces.ts
@@ -0,0 +1,15 @@
+export interface User {
+    id?: number;
+    avatar: File | null;
+    email: string;
+    name: string;
+    last_name: string;
+};
+
+export interface Token {
+    user_id: number;
+    is_staff: boolean;
+    email: string;
+    name: string;
+    last_name: string;
+};
